5d4b6dff5d5d7b1a14a823e9a1a2ec70b93e9251
sumittshrma <ss7675088@gmail.com>
2026-09-09 18:50:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ProductProvider } from './ProductContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ProductProvider>
-      <App />
-    </ProductProvider>
+    <App />
   </React.StrictMode>
 );
 
